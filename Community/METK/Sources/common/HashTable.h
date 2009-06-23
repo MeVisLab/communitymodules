47ac75c961699c54f3647584b5b870243e297980
Wolf Spindler <wolfspindler@users.sourceforge.net>
2009-06-23 06:11:55 +0000

    BUG: -Trial to fix platform dependency: Missing std namespace added before list class.

--- Community/METK/Sources/common/HashTable.h
+++ Community/METK/Sources/common/HashTable.h
@@ -3,9 +3,9 @@
 // \file    HashTable.h
 // \author  HashTable, Copyright (c) 1998 by Michael Neumann
 // \author  (Konrad Mühler)
-// \date	03.06.1998, implemented by Michael Neumann
+// \date  03.06.1998, implemented by Michael Neumann
 // \date    2005-07-05
-// 
+//
 */
 //----------------------------------------------------------------------------------
 
@@ -23,113 +23,113 @@ template<class attrType>
 class HashTable {
 public:
 
-	struct R
-	{
-		R() {}
-		R(const std::string& s, const attrType& a) : str(s), attr(a) {}
-		std::string str;
-		attrType attr;
-		bool operator  <(const R& r) {return r.str <  str;}
-		bool operator ==(const R& r) {return r.str == str;}
-	};
+  struct R
+  {
+    R() {}
+    R(const std::string& s, const attrType& a) : str(s), attr(a) {}
+    std::string str;
+    attrType attr;
+    bool operator  <(const R& r) {return r.str <  str;}
+    bool operator ==(const R& r) {return r.str == str;}
+  };
 
 
-	HashTable(int sz=2017)	// sz should be a prime
-	{
-		table = new list<R>[size=sz];
-		if(!table) throw MemoryException();
-	}
+  HashTable(int sz=2017)  // sz should be a prime
+  {
+    table = new std::list<R>[size=sz];
+    if(!table) throw MemoryException();
+  }
 
 
-	~HashTable()
-	{
-		delete[] table;
-	}
+  ~HashTable()
+  {
+    delete[] table;
+  }
 
 
-	bool insert(const std::string& str, const attrType& attr)
-	{
-		
-		int i=hash(str);
-		list<R>::iterator b;
-		b = table[i].begin();
-		while( b != table[i].end() )
-		{
-			if( (*b).str == str ) return false;
-			++b;
-		}
-		table[i].push_front(R(str,attr));
-		return true;
+  bool insert(const std::string& str, const attrType& attr)
+  {
+
+    int i=hash(str);
+    std::list<R>::iterator b;
+    b = table[i].begin();
+    while( b != table[i].end() )
+    {
+      if( (*b).str == str ) return false;
+      ++b;
+    }
+    table[i].push_front(R(str,attr));
+    return true;
     }
 
 
-	bool remove(const std::string& str)
-	{
-		int i=hash(str);
-		list<R>::iterator b;
-		b = table[i].begin();
-		while( b != table[i].end() )
-		{
-			if( (*b).str == str )
-			{
-				table[i].erase(b);
-				return true;
-			}
-			++b;
-		}
-		return false;
-	}
+  bool remove(const std::string& str)
+  {
+    int i=hash(str);
+    std::list<R>::iterator b;
+    b = table[i].begin();
+    while( b != table[i].end() )
+    {
+      if( (*b).str == str )
+      {
+        table[i].erase(b);
+        return true;
+      }
+      ++b;
+    }
+    return false;
+  }
 
 
-	attrType* find(const std::string& str)
-	{
-		int i=hash(str);
-		list<R>::iterator b;
-		b = table[i].begin();
-		while( b != table[i].end() )
-		{
-			if( (*b).str == str ) return &(*b).attr;
-			++b;
-		}
-		return 0;
-	}
+  attrType* find(const std::string& str)
+  {
+    int i=hash(str);
+    std::list<R>::iterator b;
+    b = table[i].begin();
+    while( b != table[i].end() )
+    {
+      if( (*b).str == str ) return &(*b).attr;
+      ++b;
+    }
+    return 0;
+  }
 
 
-	list<R> *getTable()
-	{
-		return table;
-	}
+  std::list<R> *getTable()
+  {
+    return table;
+  }
 
 
-	int getSize()
-	{
-		return size;
-	}
+  int getSize()
+  {
+    return size;
+  }
 
-	class MemoryException {};
+  class MemoryException {};
 
 
 
 private:
 
-	int size;
-	list<R> *table;
+  int size;
+  std::list<R> *table;
 
 
-	int hash(const std::string& str)
-	{
-		unsigned h=0, g;
-		for(size_t i=0;i<str.length();++i)
-		{
-			h = (h << 4)+ str[i];
-			if(g=h&0xf0000000)
-			{
-				h ^= g>>24;
-				h ^= g;
-			}
-		}
-		return h%size;		// size is table-size
-	}
+  int hash(const std::string& str)
+  {
+    unsigned h=0, g;
+    for(size_t i=0;i<str.length();++i)
+    {
+      h = (h << 4)+ str[i];
+      if(g=h&0xf0000000)
+      {
+        h ^= g>>24;
+        h ^= g;
+      }
+    }
+    return h%size;    // size is table-size
+  }
 
 };
 
