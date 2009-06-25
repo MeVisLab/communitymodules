cfd6379f3dd17d1a3221e5f70de8404b8b81267b
Wolf Spindler <wolfspindler@users.sourceforge.net>
2009-06-25 09:41:16 +0000

    ADD: -Main page added to doxygen reference.

---
+++ Community/General/Documentation/Sources/mlCommunitySourcesDoc.h
@@ -0,0 +1,17 @@
+// **InsertLicense** code
+//-----------------------------------------------------------------------------------
+//!
+//! Doxygen reference for the MeVisLab Community Sources.
+//!\file      mlCommunitySourcesDoc.h
+//! \section  MeVisLab Community Sources
+/*! \mainpage MeVisLab Community Sources
+ 
+This is the Doxygen reference page for the MeVisLab Community Sources. 
+It contains source code documentation automatically generated on the 
+Build Server for the Community Sources.
+
+To keep the file and class references up to date please assure that the 
+file and class comments are added to your source codes and that they are 
+activated with an "!" at the beginning of comments. 
+ 
+ */
