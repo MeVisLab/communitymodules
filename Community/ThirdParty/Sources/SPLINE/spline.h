aae5f7d67e194ec55af9456608b9d702808019f7
Wolf Spindler <wolfspindler@users.sourceforge.net>
2011-06-08 15:32:57 +0000

    MeVisLab2.1 to MeVisLab2.2: -Support for controlled library export symbols for non windows platforms added.

--- Community/ThirdParty/Sources/SPLINE/spline.h
+++ Community/ThirdParty/Sources/SPLINE/spline.h
@@ -1,28 +1,28 @@
 /**
  \file spline.h
  Interface of SPLINE library - Interpolation and Approximation of Data
- 
+
  \author Ruben Stein
  \date   2010-06-28
- 
+
  This library is a modified version of the original SPLINE library of John Burkardt found by the following link.
- 
+
  Source:     http://people.sc.fsu.edu/~jburkardt/cpp_src/spline/spline.html
  License:    GNU LESSER GENERAL PUBLIC LICENSE Version 3, 29 June 2007
              http://people.sc.fsu.edu/~jburkardt/txt/gnu_lgpl.txt
- 
+
  -------------------------------------------------------------------------------
  Description by John Burkardt:
- 
+
  SPLINE is a C++ library which constructs and evaluates spline functions.
- 
+
  These spline functions are typically used to
- 
+
  - interpolate data exactly at a set of points;
  - approximate data at many points, or over an interval.
- 
+
  -------------------------------------------------------------------------------
- 
+
  The above mentioned library was modified to be linkable as shared library. See
  below for a detailed list of changes. The resulting library is again licensed
  using LGPLv3.
@@ -51,23 +51,35 @@
 //stl
 #include <string>
 
-//! \def SPLINE_EXPORT Defines import/export macros necessary to use a shared
-//!                    library interface on windows platform.
-#ifdef _WIN32
+//! \def SPLINE_EXPORT Defines library import/export macros necessary to use a shared
+//!                    library interface on some platforms.
+#if defined (ML_LIBRARY_EXPORT_ATTRIBUTE) || defined (ML_LIBRARY_IMPORT_ATTRIBUTE)
+  // Make use of the new style and platform independent import/export definitions.
   #ifdef SPLINE_EXPORTS
-    #define SPLINE_EXPORT __declspec(dllexport)
+    // Export library symbols.
+    #define SPLINE_EXPORT   ML_LIBRARY_EXPORT_ATTRIBUTE
   #else
-    #define SPLINE_EXPORT __declspec(dllimport)
+    // If included by external modules, exported symbols are declared as import symbols
+    #define SPLINE_EXPORT   ML_LIBRARY_IMPORT_ATTRIBUTE
   #endif
 #else
-  #define SPLINE_EXPORT
+  // Only for backward compatibility to versions < MeVisLab2.2.
+  #ifdef _WIN32
+    #ifdef SPLINE_EXPORTS
+      #define SPLINE_EXPORT __declspec(dllexport)
+    #else
+      #define SPLINE_EXPORT __declspec(dllimport)
+    #endif
+  #else
+    #define SPLINE_EXPORT
+  #endif
 #endif
 
 //------------------------------------------------------------------------------
 namespace spline
 {
 SPLINE_EXPORT double basis_function_b_val ( double tdata[], double tval );
-SPLINE_EXPORT double basis_function_beta_val ( double beta1, double beta2, double tdata[], 
+SPLINE_EXPORT double basis_function_beta_val ( double beta1, double beta2, double tdata[],
   double tval );
 SPLINE_EXPORT double *basis_matrix_b_uni ( );
 SPLINE_EXPORT double *basis_matrix_beta_uni ( double beta1, double beta2 );
@@ -79,7 +91,7 @@ SPLINE_EXPORT double *basis_matrix_overhauser_nur ( double beta );
 SPLINE_EXPORT double *basis_matrix_overhauser_uni ( void);
 SPLINE_EXPORT double *basis_matrix_overhauser_uni_l ( );
 SPLINE_EXPORT double *basis_matrix_overhauser_uni_r ( );
-SPLINE_EXPORT double basis_matrix_tmp ( int left, int n, double mbasis[], int ndata, 
+SPLINE_EXPORT double basis_matrix_tmp ( int left, int n, double mbasis[], int ndata,
   double tdata[], double ydata[], double tval );
 SPLINE_EXPORT void bc_val ( int n, double t, double xcon[], double ycon[], double *xval,
   double *yval );
@@ -99,16 +111,16 @@ SPLINE_EXPORT void data_to_dif ( int ntab, double xtab[], double ytab[], double 
 SPLINE_EXPORT double dif_val ( int ntab, double xtab[], double diftab[], double xval );
 SPLINE_EXPORT int i4_max ( int i1, int i2 );
 SPLINE_EXPORT int i4_min ( int i1, int i2 );
-SPLINE_EXPORT void least_set ( int point_num, double x[], double f[], double w[], 
+SPLINE_EXPORT void least_set ( int point_num, double x[], double f[], double w[],
   int nterms, double b[], double c[], double d[] );
-SPLINE_EXPORT double least_val ( int nterms, double b[], double c[], double d[], 
+SPLINE_EXPORT double least_val ( int nterms, double b[], double c[], double d[],
   double x );
-SPLINE_EXPORT void least_val2 ( int nterms, double b[], double c[], double d[], double x, 
+SPLINE_EXPORT void least_val2 ( int nterms, double b[], double c[], double d[], double x,
   double *px, double *pxp );
-SPLINE_EXPORT void least_set_old ( int ntab, double xtab[], double ytab[], int ndeg, 
+SPLINE_EXPORT void least_set_old ( int ntab, double xtab[], double ytab[], int ndeg,
   double ptab[], double b[], double c[], double d[], double *eps, int *ierror );
 SPLINE_EXPORT double least_val_old ( double x, int ndeg, double b[], double c[], double d[] );
-SPLINE_EXPORT void parabola_val2 ( int ndim, int ndata, double tdata[], double ydata[], 
+SPLINE_EXPORT void parabola_val2 ( int ndim, int ndata, double tdata[], double ydata[],
   int left, double tval, double yval[] );
 SPLINE_EXPORT double pchst ( double arg1, double arg2 );
 SPLINE_EXPORT double r8_max ( double x, double y );
@@ -129,32 +141,32 @@ SPLINE_EXPORT double spline_b_val ( int ndata, double tdata[], double ydata[], d
 SPLINE_EXPORT double spline_beta_val ( double beta1, double beta2, int ndata, double tdata[],
   double ydata[], double tval );
 SPLINE_EXPORT double spline_constant_val ( int ndata, double tdata[], double ydata[], double tval );
-SPLINE_EXPORT double *spline_cubic_set ( int n, double t[], double y[], int ibcbeg, double ybcbeg, 
+SPLINE_EXPORT double *spline_cubic_set ( int n, double t[], double y[], int ibcbeg, double ybcbeg,
   int ibcend, double ybcend );
 SPLINE_EXPORT double spline_cubic_val ( int n, double t[], double tval, double y[], double ypp[],
   double *ypval, double *yppval );
-SPLINE_EXPORT void spline_cubic_val2 ( int n, double t[], double tval, int *left, double y[], 
+SPLINE_EXPORT void spline_cubic_val2 ( int n, double t[], double tval, int *left, double y[],
   double ypp[], double *yval, double *ypval, double *yppval );
-SPLINE_EXPORT double *spline_hermite_set ( int ndata, double tdata[], double ydata[], 
+SPLINE_EXPORT double *spline_hermite_set ( int ndata, double tdata[], double ydata[],
   double ypdata[] );
-SPLINE_EXPORT void spline_hermite_val ( int ndata, double tdata[], double c[], double tval, 
+SPLINE_EXPORT void spline_hermite_val ( int ndata, double tdata[], double c[], double tval,
   double *sval, double *spval );
-SPLINE_EXPORT double spline_linear_int ( int ndata, double tdata[], double ydata[], double a, 
+SPLINE_EXPORT double spline_linear_int ( int ndata, double tdata[], double ydata[], double a,
   double b );
-SPLINE_EXPORT void spline_linear_intset ( int int_n, double int_x[], double int_v[], 
+SPLINE_EXPORT void spline_linear_intset ( int int_n, double int_x[], double int_v[],
   double data_x[], double data_y[] );
-SPLINE_EXPORT void spline_linear_val ( int ndata, double tdata[], double ydata[], 
+SPLINE_EXPORT void spline_linear_val ( int ndata, double tdata[], double ydata[],
   double tval, double *yval, double *ypval );
-SPLINE_EXPORT double spline_overhauser_nonuni_val ( int ndata, double tdata[], 
+SPLINE_EXPORT double spline_overhauser_nonuni_val ( int ndata, double tdata[],
   double ydata[], double tval );
 SPLINE_EXPORT double spline_overhauser_uni_val ( int ndata, double tdata[], double ydata[],
   double tval );
-SPLINE_EXPORT void spline_overhauser_val ( int ndim, int ndata, double tdata[], double ydata[], 
+SPLINE_EXPORT void spline_overhauser_val ( int ndim, int ndata, double tdata[], double ydata[],
   double tval, double yval[] );
 SPLINE_EXPORT void spline_pchip_set ( int n, double x[], double f[], double d[] );
-SPLINE_EXPORT void spline_pchip_val ( int n, double x[], double f[], double d[], int ne, 
+SPLINE_EXPORT void spline_pchip_val ( int n, double x[], double f[], double d[], int ne,
   double xe[], double fe[] );
-SPLINE_EXPORT void spline_quadratic_val ( int ndata, double tdata[], double ydata[], 
+SPLINE_EXPORT void spline_quadratic_val ( int ndata, double tdata[], double ydata[],
   double tval, double *yval, double *ypval );
 SPLINE_EXPORT void timestamp ( );
 } //namespace spline
