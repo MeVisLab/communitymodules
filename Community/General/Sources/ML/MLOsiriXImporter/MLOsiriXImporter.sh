ef6a5c2e0f2fd785b5c8ebd84f365e14d2a153d2
Felix Ritter <awakeideas@users.sourceforge.net>
2014-07-20 11:59:40 +0000

    removed obsolete file

--- Community/General/Sources/ML/MLOsiriXImporter/MLOsiriXImporter.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-if [ "`uname -s`" = "Darwin" && "$MLAB_ROOT" = "" ]; then
-  echo "Generate Xcode projects by double-clicking the file 'MLOsiriXImporter.pro' in Finder or use the MeVisLabProjectGenerator.app from the command line to customize the project creation."
-else
-  python $MLAB_ROOT/MeVis/Foundation/BuildTools/Scripts/createProject.py MLOsiriXImporter
-fi
